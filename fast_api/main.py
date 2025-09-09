e69007d6b07af833a078f28fde028a57c028c2a1
Pranay Gundu <pranaygundu423@gmail.com>
2025-09-09 18:09:39 +0530

    small Changes

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fast_api.model_calling import generate_summary
+from fast_api.model_calling import ModelCalling
 
 # Request body schema
 class InputSchema(BaseModel):
@@ -8,11 +8,11 @@ class InputSchema(BaseModel):
 
 # Create app
 app = FastAPI()
-
+modelcalling = ModelCalling()
 # POST endpoint
 @app.post("/summarize")
 def summarizer(data: InputSchema):
-    response , parsed_output , summary = generate_summary(url=data.input_url)
+    response , parsed_output , summary = modelcalling.generate_summary(url=data.input_url)
     return {
         "summary": summary,
         "parsed_output": parsed_output,
--- fast_api/model_calling.py
+++ fast_api/model_calling.py
@@ -1,10 +1,14 @@
 
 from testing_summary.model_langChain import YouTubeVideoSummarizer
 from testing_chatbot.rag.utils_youtube import load_transcript
-
-def generate_summary(url : str)-> str:
-    transcripts = load_transcript(url=url)
-    summarizer= YouTubeVideoSummarizer()
-    response , parsed_output , summary = summarizer.summarize_video(transcripts)
-    #formatted_summary = summarizer.format_summary_output(summary=summary)
-    return response , parsed_output , summary
+from typing import Tuple
+from langchain_core.messages.ai import AIMessage
+class ModelCalling:
+    def __init__(self):
+        pass
+    
+    def generate_summary(self, url: str) -> Tuple[AIMessage, "VideoSummary", dict]:
+        transcripts = load_transcript(url=url)
+        summarizer = YouTubeVideoSummarizer()
+        response, parsed_output, summary = summarizer.summarize_video(transcripts)
+        return response, parsed_output, summary
